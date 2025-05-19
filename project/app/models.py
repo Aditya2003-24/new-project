affa2544bcdbf903e8d3fb4b2f7e012ce66848b9
Aditya yadav <adityayadavadi6100@gmail.com>
2025-05-19 15:57:08 +0530

    new code

---
+++ project/app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Employee(models.Model):
+    emp_name=models.CharField(max_length=40)
+    emp_email=models.EmailField()
+    emp_Joining=models.DateField()
+    emp_dep=models.CharField(max_length=40)
+    emp_contact=models.IntegerField()
+    emp_age=models.IntegerField()
+    emp_image=models.ImageField(upload_to='image/')
+    emp_password=models.IntegerField()
+
---
+++ project/app/views.py
@@ -0,0 +1,114 @@
+from django.shortcuts import render
+from .models import *
+
+# Create your views here.
+def landing(request):
+    return render(request,'landing.html')
+def login(request):
+    email=request.POST.get('email')
+    passw=request.POST.get('password')
+    
+    if request.method=='POST':
+        adminname='admin@gmail.com'
+        adminpass='2040'
+
+        if email==adminname and passw==adminpass:
+            return render(request,'admin.html',{'admindata':adminname})
+        
+    if request.method=='POST':
+
+            user=Employee.objects.get(emp_email=email)
+            if user:
+                
+                userdata=Employee.objects.get(emp_email=email)
+                pass2=userdata.emp_password
+                print(userdata.emp_password)
+                print(userdata)
+                print(passw)
+
+                if passw==pass2:
+                    print('helo')
+                    return render(request,'employee.html',{'userdata':userdata})
+                else:
+                    return render(request,'employee.html',{'userdata':userdata})
+
+                
+                
+    else:
+        return render(request,'login.html')
+    
+def register(request):
+    adminname='admin@gmail.com'
+    aditya='admin aditya yadav'
+    if request.method=='POST':
+        if request.method=='POST':
+  
+           username=request.POST.get('empName')
+           email=request.POST.get('email')
+           department=request.POST.get('department')
+           phone=request.POST.get('contact')
+           age=request.POST.get('age')
+           joinDate=request.POST.get('joinDate')
+           image=request.FILES.get('file')
+           password=request.POST.get('password')
+         
+   
+
+           u=Employee.objects.filter(emp_email=email)
+           if u:
+               msg='Email already exist'
+               return render(request,'admin.html',{'key':msg,'admindata':adminname,'aditya':aditya})
+           else:
+             if password==password:
+               Employee.objects.create(emp_name=username,emp_email=email,emp_Joining=joinDate,emp_dep=department,emp_contact=phone,emp_age=age,emp_image=image,emp_password=password)
+               msg='registration done'
+               return render(request,'admin.html',{'key':msg,'admindata':adminname,'aditya':aditya})
+             else:
+                msg1='pssword and cpssword not match'
+                return render(request,'admin.html',{'key':msg1,'admindata':adminname,'aditya':aditya})
+         
+    else:
+         return render(request,'admin.html',{'admindata':adminname,'aditya':aditya}) 
+def alldata(request):
+    data=Employee.objects.all()
+    adminname='admin@gmail.com'
+    return render(request,'admin.html',{'data':data,'admindata':adminname})
+
+def editdata(request,pk):
+    adminname='admin@gmail.com'
+    data=Employee.objects.all()
+    editdata=Employee.objects.get(id=pk)
+    return render(request,'admin.html',{'editdata':editdata,'admindata':adminname,'data':data})
+
+def edit(request,pk):
+    username=request.POST.get('empName')
+    email=request.POST.get('email')
+    department=request.POST.get('department')
+    phone=request.POST.get('contact')
+    age=request.POST.get('age')
+    joinDate=request.POST.get('joinDate')
+           
+    if request.method=='POST':
+        edit=Employee.objects.get(id=pk)
+        edit.emp_name=username
+        edit.emp_email=email
+        edit.emp_Joining=joinDate
+        edit.emp_dep=department
+        edit.emp_contact=phone
+        edit.emp_age=age
+        edit.save()
+        adminname='admin@gmail.com'
+        data=Employee.objects.all()
+        return render(request,'admin.html',{'admindata':adminname,'data':data})
+    
+def delete(request,pk):
+    delet=Employee.objects.get(id=pk)
+    delet.delete()
+    adminname='admin@gmail.com'
+    data=Employee.objects.all()
+    return render(request,'admin.html',{'admindata':adminname,'data':data})
+
+        
+def profile(request,pk):
+    userdata=Employee.objects.get(id=pk)
+    return render(request,'profile.html',{'userdata':userdata})
